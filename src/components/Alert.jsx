e1b0322dfbcb0cee867acfae700a7c443e6c0db6
MINT <mint_chanit@hotmail.com>
2025-03-18 15:09:19 +0700

    feat(alert, button): add styling with css prop

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,45 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react"
 
-function Alert() {}
+function Alert({type, message}) {
+    const baseStyle = css`
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        color: black;
+        font-size: 18px;
+        font-weight: bold;
+        text-align: left;
+        border: none;
+        border-radius: 8px;
+        padding: 16px;
+        padding-right: 300px;
+        margin: 40px;
+        box-sizing: border-box;`;
+
+    const errorStyle = css`
+        ${baseStyle};
+        background-color: #FFCCCC`;
+
+    const warningStyle = css`
+        ${baseStyle};
+        background-color: #FFCC99`;
+
+    const infoStyle = css`
+        ${baseStyle};
+        background-color: #FFFFCC`;
+
+    const successStyle = css`
+        ${baseStyle};
+        background-color:rgb(190, 246, 190)`;
+    
+    return (
+        <p css=
+            {type === "error" ? 
+            errorStyle: type === "warning" ?
+            warningStyle: type === "info" ?
+            infoStyle: successStyle}>{message}</p>
+    )
+}
+
+export default Alert;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,53 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react"
 
-function Button() {}
+function Button({ type }) {
+    const baseStyle = css`
+        display: flex;
+        color: white;
+        font-size: 16px;
+        border-radius: 4px;
+        padding: 10px 60px;
+        border: none;
+        margin: 40px;
+        &:hover {
+            color: black;
+            }
+        `;
+
+    const primaryStyle = css`
+        ${baseStyle};
+        background-color: #074EE8;
+        &:hover {
+            background-color:rgb(144, 173, 236);
+            }`;
+
+    const secondaryStyle = css`
+        ${baseStyle};
+        background-color: #07A4E8;
+        &:hover {
+            background-color:rgb(146, 207, 235);
+            }`;
+
+    const errorStyle = css`
+        ${baseStyle};
+        background-color: #DE5753;
+        &:hover {
+            background-color:rgb(237, 158, 155);
+            }`;
+
+    const successStyle = css`
+        ${baseStyle};
+        background-color: #26B795;
+        &:hover {
+            background-color:rgb(148, 235, 214);
+            }`;
+    
+    return (
+        <button css={type === "primary" ? 
+            primaryStyle : type === "secondary" ? secondaryStyle: type === "error" ? 
+            errorStyle: successStyle}>Button</button>
+    )
+}
+
+export default Button;
